68f635c1ac3c92d14e790ad6999069d6b9a0dcf0
Moshe Shamash <mshamash@fb.com>
2020-01-28 12:08:31 +0000

    First commit

---
+++ src/AdminLogin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import "./AdminLogin.css";
+
+function AdminLogin(setIsVolunteerLogin) {
+  return (
+    <div className="adminLoginWrapper">
+      <div className="title">Orgainser Login</div>
+    </div>
+  );
+}
+
+export default AdminLogin;
---
+++ src/VolunteerLogin.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+
+function VolunteerLogin() {
+  return <div className="volunteerLoginWrapper"></div>;
+}
+
+export default VolunteerLogin;
